7322701f9b286d4eabea19e3ef0c7f93075667b4
Greg Lindahl <lindahl@pbm.com>
2022-06-29 19:08:16 +0000

    pennsic49

---
+++ pennsic49/reformat.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import sys
+
+contents = ''
+with open(sys.argv[1], 'r') as f:
+    contents = f.read()
+
+# there is exactly one table in this file
+
+assert contents.count('<table ') == 1
+
+contents = contents[contents.find('<table '):]
+contents = contents[contents.find('<tbody>')+7:]
+contents = contents[:contents.find('</table>')+8:]
+contents = contents.replace('\n', ' ').replace('</tr>', '</tr>\n')
+contents = contents.replace('&#39;', "'")
+
+with open('classes-all', 'w') as f:
+    print(contents, file=f)
+
